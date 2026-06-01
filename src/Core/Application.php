8ef3f9431031ab4aa36a8d3aca17c41279030813
Minhao Zhang <minhao.zhang@qq.com>
2026-06-01 17:55:11 +0800

    *(version) by Kiro: bump version to 0.7.6

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -31,7 +31,7 @@ final class Application
     ): SymfonyApplication {
         $checker = new CheckService();
 
-        $app = new SymfonyApplication('apm', '0.7.5');
+        $app = new SymfonyApplication('apm', '0.7.6');
         $app->setDefaultCommand('list');
         ConsoleRegistration::register($app, $installer, $checker, $updater, $presetRegistry);
 
